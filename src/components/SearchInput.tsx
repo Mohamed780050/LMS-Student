ba46c74d5debdf9a711bdcefc91a87d48cdec546
Mohamed780050 <mohamed39omer@gmail.com>
2024-11-21 12:19:11 +0200

    chaged the input for searching and made it appear in the url

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,22 +1,20 @@
-import { useDispatch } from "react-redux";
 import { Input } from "./ui/input";
-import { changeCourseTitle } from "@/Redux/global";
 import { useRef } from "react";
+import { useSearchParams } from "react-router-dom";
 
 function SearchInput() {
+  const [searchParams, setSearchParams] = useSearchParams();
   const courseTitle = useRef<HTMLInputElement | null>(null);
-  const dispatch = useDispatch();
   return (
     <div className="md:pl-56 min-w-64 md:min-w-96">
       <Input
         ref={courseTitle}
         placeholder="search for....."
         onChange={() => {
-          const timer = setTimeout(() => {
-            console.log(courseTitle.current?.value);
-            dispatch(changeCourseTitle(`${courseTitle.current?.value}`));
-            return () => clearTimeout(timer);
-          }, 1500);
+          setSearchParams({
+            ...Object.fromEntries(searchParams.entries()),
+            title: `${courseTitle.current?.value}`,
+          });
         }}
       />
     </div>
