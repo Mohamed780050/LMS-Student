24fca11e9fc73058df1d65500fc12154d94bc18f
Mohamed780050 <mohamed39omer@gmail.com>
2024-11-24 14:24:13 +0200

    finished work on the side bar as a UI

--- src/Layout/CoursePageLayout.tsx
+++ src/Layout/CoursePageLayout.tsx
@@ -6,17 +6,17 @@ import { useParams } from "react-router-dom";
 
 function CoursePageLayout() {
   const { id } = useParams();
-  const {  isLoading } = useQuery({
+  const { isLoading } = useQuery({
     queryKey: [""],
     queryFn: () => getACourse(`${id}`),
   });
   return (
     <div className="h-full">
       <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
-         <CourseSidebar isLoading={isLoading}/>
+        <CourseSidebar isLoading={isLoading} />
       </div>
       <main className="md:pl-80 h-full">
-         <CoursePage isLoading={isLoading}/>
+        <CoursePage isLoading={isLoading} />
       </main>
     </div>
   );
--- src/components/CourseSidebarChapters.tsx
+++ src/components/CourseSidebarChapters.tsx
@@ -1,12 +1,35 @@
 import { RootState } from "@/Redux/store";
+import { Lock } from "lucide-react";
 import { useSelector } from "react-redux";
 
 function CourseSidebarChapters() {
-  const {chapters} = useSelector((state:RootState) => state.chapters)
+  const { chapters } = useSelector((state: RootState) => state.chapters);
   return (
     <>
-      <ul className="flex flex-col w-full">
-        {chapters.map(chapter => <li>{chapter.chapterName}</li>)}
+      <ul className="flex flex-col w-full gap-2">
+        {chapters.map((chapter) => (
+          <li
+            className={`flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20 cursor-pointer ${
+              // chapter.active &&
+              // "text-slate-700 bg-slate-200/20 hover:bg-slate-200/20 hover:text-slate-700"
+              ""
+              // TODO: make sure to add active to chapter and isCompleted.
+              // chapter.isCompleted && "text-emerald-700 hover:text-emerald-700"
+              // chapter.isCompleted && chapter.active && "bg-emerald-200/20"
+            }`}
+          >
+            <div
+              className={`flex items-center gap-x-2 py-4 ${
+                // chapter.active && "text-slate-700"
+                // chapter.isCompleted && "text-emerald-700"
+                ""
+              }`}
+            >
+              <Lock size={22} />
+              <p>{chapter.chapterName}</p>
+            </div>
+          </li>
+        ))}
       </ul>
     </>
   );
