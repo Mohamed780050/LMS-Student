0d4aff5696fa6dbcbf59cf69ced39a21b862556d
Mohamed780050 <mohamed39omer@gmail.com>
2024-11-28 10:09:18 +0200

    added the link componete to the chapters

--- src/Routes/pages/CoursePage.tsx
+++ src/Routes/pages/CoursePage.tsx
@@ -1,9 +1,6 @@
 import { Skeleton } from "@/components/ui/skeleton";
-import getACourse from "@/config/getACourse";
 import { RootState } from "@/Redux/store";
-import { useQuery } from "@tanstack/react-query";
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
 
 function CoursePage({ isLoading }: { isLoading: boolean }) {
   const { courseName } = useSelector(
--- src/components/CourseSidebarChapters.tsx
+++ src/components/CourseSidebarChapters.tsx
@@ -1,6 +1,7 @@
 import { RootState } from "@/Redux/store";
 import { Lock, PlayCircle } from "lucide-react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 function CourseSidebarChapters() {
   const { chapters } = useSelector((state: RootState) => state.chapters);
@@ -8,27 +9,29 @@ function CourseSidebarChapters() {
     <>
       <ul className="flex flex-col w-full gap-2">
         {chapters.map((chapter) => (
-          <li
-            className={`flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20 cursor-pointer ${
-              // chapter.active &&
-              // "text-slate-700 bg-slate-200/20 hover:bg-slate-200/20 hover:text-slate-700"
-              ""
-              // TODO: make sure to add active to chapter and isCompleted.
-              // chapter.isCompleted && "text-emerald-700 hover:text-emerald-700"
-              // chapter.isCompleted && chapter.active && "bg-emerald-200/20"
-            }`}
-          >
-            <div
-              className={`flex items-center gap-x-2 py-4 ${
-                // chapter.active && "text-slate-700"
-                // chapter.isCompleted && "text-emerald-700"
+          <Link to={`chapter/${chapter._id}`}>
+            <li
+              className={`flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20 cursor-pointer ${
+                // chapter.active &&
+                // "text-slate-700 bg-slate-200/20 hover:bg-slate-200/20 hover:text-slate-700"
                 ""
+                // TODO: make sure to add active to chapter and isCompleted.
+                // chapter.isCompleted && "text-emerald-700 hover:text-emerald-700"
+                // chapter.isCompleted && chapter.active && "bg-emerald-200/20"
               }`}
             >
-              {chapter.isFree ? <PlayCircle size={22} /> : <Lock size={22} />}
-              <p>{chapter.chapterName}</p>
-            </div>
-          </li>
+              <div
+                className={`flex items-center gap-x-2 py-4 ${
+                  // chapter.active && "text-slate-700"
+                  // chapter.isCompleted && "text-emerald-700"
+                  ""
+                }`}
+              >
+                {chapter.isFree ? <PlayCircle size={22} /> : <Lock size={22} />}
+                <p>{chapter.chapterName}</p>
+              </div>
+            </li>
+          </Link>
         ))}
       </ul>
     </>
